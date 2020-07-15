6c1eae6dfa8da6e667a288f6d5a10d0600af0d64
B0218966 <b0218966@LTB0218966-Mac.local>
2020-07-15 17:30:12 +0530

    Updates

---
+++ precache-manifest.8f243a4908a099f520042d4d95dff6b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36a2845c4adb55a60910a0ce2905ec7",
+    "url": "/ReactAssignment/index.html"
+  },
+  {
+    "revision": "57aba3cbf7aed8afb783",
+    "url": "/ReactAssignment/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "47d7df5835718f5b8bd6",
+    "url": "/ReactAssignment/static/css/main.bb3c984e.chunk.css"
+  },
+  {
+    "revision": "57aba3cbf7aed8afb783",
+    "url": "/ReactAssignment/static/js/2.36d3b407.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactAssignment/static/js/2.36d3b407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47d7df5835718f5b8bd6",
+    "url": "/ReactAssignment/static/js/main.63cca053.chunk.js"
+  },
+  {
+    "revision": "95ae7c30126044cb9b4a",
+    "url": "/ReactAssignment/static/js/runtime-main.62ad5f2a.js"
+  }
+]);
